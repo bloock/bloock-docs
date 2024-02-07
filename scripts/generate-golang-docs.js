6cd260cce495d69a7bbde90ff5cc1a3d79f52903
marcbaque <marc@bloock.com>
2024-02-07 11:53:27 +0100

    add golang ref

---
+++ scripts/generate-golang-docs.js
@@ -0,0 +1,12 @@
+const fs = require('fs-extra');
+const path = require('path');
+
+const dest = './docs/sdks/golang/reference';
+const category = {
+  position: 3,
+  label: 'Reference',
+  collapsible: true,
+};
+
+const categoryPath = path.join(dest, '_category_.json');
+fs.outputFileSync(categoryPath, JSON.stringify(category));
